c603531a4c2121f9d89aa393e985422740141b27
Ugarm <ugo.armer@gmail.com>
2024-11-20 01:17:14 +0100

    Main cleanup

--- src/main/java/org/ugarm/zupbagarre/Main.java
+++ src/main/java/org/ugarm/zupbagarre/Main.java
@@ -1,13 +1,8 @@
 package org.ugarm.zupbagarre;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Main {
-    private static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.debug("looooool");
         Game game = new Game();
         game.game();
     }
